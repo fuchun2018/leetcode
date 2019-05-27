a6e3b89169fa7cc7bb1accdcf3c38e29589cf59a
fuchun2018 <russel.chang@gmail.com>
2019-05-26 23:58:44 -0700

    init

---
+++ leetcode/WildcardMatch/WildcardMatchSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.WildcardMatch
+{
+    public class WildcardMatchSolution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            return false;
+        }
+    }
+}
---
+++ leetcodeTests/WildcardMatch/WildcardMatchSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.WildcardMatch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.WildcardMatch.Tests
+{
+    [TestClass()]
+    public class WildcardMatchSolutionTests
+    {
+        [TestMethod()]
+        public void IsMatchTest()
+        {
+            var solution = new WildcardMatchSolution();
+            var result = solution.IsMatch(string.Empty, string.Empty);
+            Assert.IsFalse(result);
+        }
+    }
+}
