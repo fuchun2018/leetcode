640493f7cbcea614365af032fdd997d76eac799f
fuchun2018 <russel.chang@gmail.com>
2019-05-28 13:03:47 -0700

    init "29.Divide Two Integer"

---
+++ leetcode/DivideTwoIntegers/DivideSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.DivideTwoIntegers
+{
+    public class DivideSolution
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            return -1;
+        }
+    }
+}
---
+++ leetcodeTests/DivideTwoIntegers/DivideSolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.DivideTwoIntegers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.DivideTwoIntegers.Tests
+{
+    [TestClass()]
+    public class DivideSolutionTests
+    {
+        [TestMethod()]
+        public void DivideTest()
+        {
+            var solution = new DivideSolution();
+
+            var result = solution.Divide(10, 3);
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
