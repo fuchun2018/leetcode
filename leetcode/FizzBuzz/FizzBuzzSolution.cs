f266490873beb947104c316ffca5f6bc004d119a
fuchun2018 <russel.chang@gmail.com>
2019-05-29 11:37:47 -0700

    init 412. Fizz Buzz

---
+++ leetcode/FizzBuzz/FizzBuzzSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.FizzBuzz
+{
+    public class FizzBuzzSolution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            return null;
+        }
+    }
+}
---
+++ leetcodeTests/FizzBuzz/FizzBuzzSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.FizzBuzz.Tests
+{
+    [TestClass()]
+    public class FizzBuzzSolutionTests
+    {
+        [TestMethod()]
+        public void FizzBuzzTest()
+        {
+            var solution = new FizzBuzzSolution();
+            var result = solution.FizzBuzz(0);
+            Assert.IsNotNull(result);
+        }
+    }
+}
