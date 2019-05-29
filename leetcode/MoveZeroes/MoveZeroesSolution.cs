ffb7b8c448a4e38c587c3821afe59bf04bd10b6f
fuchun2018 <russel.chang@gmail.com>
2019-05-29 12:36:21 -0700

    init 283. Move Zeroes

---
+++ leetcode/MoveZeroes/MoveZeroesSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.MoveZeroes
+{
+    public class MoveZeroesSolution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+
+        }
+    }
+}
---
+++ leetcodeTests/MoveZeroes/MoveZeroesSolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.MoveZeroes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.MoveZeroes.Tests
+{
+    [TestClass()]
+    public class MoveZeroesSolutionTests
+    {
+        [TestMethod()]
+        public void MoveZeroesTest()
+        {
+            var solution = new MoveZeroesSolution();
+             solution.MoveZeroes(new int[] { 0, 1, 0, 3, 12 });
+        }
+    }
+}
