0c259a31976221bd64b23611b3bb50fadefbe76b
fuchun2018 <russel.chang@gmail.com>
2019-05-29 12:12:35 -0700

    third attemption, use string array instead. it faster than string list: 83.86 runtime and 79.83 memory usage.

--- leetcode/FizzBuzz/FizzBuzzSolution.cs
+++ leetcode/FizzBuzz/FizzBuzzSolution.cs
@@ -10,14 +10,14 @@ namespace leetcode.FizzBuzz
     {
         public IList<string> FizzBuzz(int n)
         {
-            if (n == 0) return new List<string>();
-            var result = new List<string>();
+            if (n == 0) return new string[] { };
+            var result = new string[n];
             for (int index = 1; index <= n; index++)
             {
-                if (index % (3 * 5) == 0) result.Add("FizzBuzz");
-                else if (index % 3 == 0) result.Add("Fizz");
-                else if (index % 5 == 0) result.Add("Buzz");
-                else result.Add(index.ToString());
+                if (index % (3 * 5) == 0) result[index-1]  = "FizzBuzz";
+                else if (index % 3 == 0) result[index-1] ="Fizz";
+                else if (index % 5 == 0) result[index-1] = "Buzz";
+                else result[index-1] = index.ToString();
             }
             return result;
         }
