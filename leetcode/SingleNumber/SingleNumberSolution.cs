18afab654c4520ee4eee7a819ebae5be40e2b483
fuchun2018 <russel.chang@gmail.com>
2019-05-29 11:10:51 -0700

    init 136. Single Number

---
+++ leetcode/SingleNumber/SingleNumberSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.SingleNumber
+{
+    public class SingleNumberSolution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            return -1;
+        }
+    }
+}
---
+++ leetcodeTests/SingleNumber/SingleNumberSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.SingleNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.SingleNumber.Tests
+{
+    [TestClass()]
+    public class SingleNumberSolutionTests
+    {
+        [TestMethod()]
+        public void SingleNumberTest()
+        {
+            var solution = new SingleNumberSolution();
+            var result = solution.SingleNumber(new int[] { 4, 1, 2, 1, 2 });
+            Assert.AreEqual(result, 4);
+        }
+    }
+}
