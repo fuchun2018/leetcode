fd5868ca4478eb76b3f7ad3371f581105e75819b
fuchun2018 <russel.chang@gmail.com>
2019-05-29 10:46:09 -0700

    init Reverse String

---
+++ leetcodeTests/ReverseString/ReverseStringSolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.ReverseString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.ReverseString.Tests
+{
+    [TestClass()]
+    public class ReverseStringSolutionTests
+    {
+        [TestMethod()]
+        public void ReverseStringTest()
+        {
+            var solution = new ReverseStringSolution();
+            solution.ReverseString("hello".ToCharArray());
+        }
+    }
+}
