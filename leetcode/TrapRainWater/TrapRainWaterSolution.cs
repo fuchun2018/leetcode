4caed04f211f331f78402e7fd108f58c16136c9c
fuchun2018 <russel.chang@gmail.com>
2019-05-30 15:58:55 -0700

    init

---
+++ leetcode/TrapRainWater/TrapRainWaterSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.TrapRainWater
+{
+    public class TrapRainWaterSolution
+    {
+        public int Trap(int[] height)
+        {
+            return 0;
+        }
+    }
+}
---
+++ leetcodeTests/TrapRainWater/TrapRainWaterSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.TrapRainWater;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.TrapRainWater.Tests
+{
+    [TestClass()]
+    public class TrapRainWaterSolutionTests
+    {
+        [TestMethod()]
+        public void TrapTest()
+        {
+            var solution = new TrapRainWaterSolution();
+            var input = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            var result = solution.Trap(input);
+            Assert.AreEqual(result, 6);
+            Assert.Fail();
+        }
+    }
+}
