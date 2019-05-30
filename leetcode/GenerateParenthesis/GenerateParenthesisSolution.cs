bcedadb5d358819345a85c5dfef301517e6114cd
fuchun2018 <russel.chang@gmail.com>
2019-05-30 09:10:17 -0700

    first attempion, passed all test cased from leetcode, 12.61/100, 47.88/100

---
+++ leetcode/GenerateParenthesis/GenerateParenthesisSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.GenerateParenthesis
+{
+    public class GenerateParenthesisSolution
+    {
+        private int max;
+        public IList<string> GenerateParenthesis(int n)
+        {
+            if (n == 0) return new List<string>() { "" };
+            max = n;
+            HashSet<string>  result = new HashSet<string>();
+            ParenthesisGenerator(0, 0, "", result);
+            return result.ToList();
+        }
+
+        private void ParenthesisGenerator(int left, int right, string wip, HashSet<string> list)
+        {
+            //terminal condition
+            if ((left > max) || (right> max) || (right > left)) return;
+            if (left == max && right == max)
+            {
+                if (list.Contains(wip) == false)
+                    list.Add(wip);
+                return;
+            }
+
+            ParenthesisGenerator(left + 1, right, wip + "(", list);
+            ParenthesisGenerator(left, right + 1, wip + ")", list);
+        }
+    }
+}
---
+++ leetcodeTests/GenerateParenthesis/GenerateParenthesisSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.GenerateParenthesis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.GenerateParenthesis.Tests
+{
+    [TestClass()]
+    public class GenerateParenthesisSolutionTests
+    {
+        [TestMethod()]
+        public void GenerateParenthesisTest()
+        {
+            var solution = new GenerateParenthesisSolution();
+            var result = solution.GenerateParenthesis(5);
+            Assert.IsNotNull(result);
+        }
+    }
+}
