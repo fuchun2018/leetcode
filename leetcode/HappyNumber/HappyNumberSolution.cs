02ac1497910788d7bb573fa6c503db5af6542228
fuchun2018 <russel.chang@gmail.com>
2019-05-30 12:01:57 -0700

    inital

---
+++ leetcode/HappyNumber/HappyNumberSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.HappyNumber
+{
+    public class HappyNumberSolution
+    {
+        public bool IsHappy(int n)
+        {
+            return false;
+        }
+    }
+}
---
+++ leetcodeTests/HappyNumber/HappyNumberSolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.HappyNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.HappyNumber.Tests
+{
+    [TestClass()]
+    public class HappyNumberSolutionTests
+    {
+        [TestMethod()]
+        public void IsHappyTest()
+        {
+
+            Assert.IsTrue(new HappyNumberSolution().IsHappy(19));
+        }
+    }
+}
