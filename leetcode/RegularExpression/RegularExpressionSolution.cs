b1e129e3979bbf525e70404397ef15426fc49f8e
fuchun2018 <russel.chang@gmail.com>
2019-06-01 10:08:42 -0700

    initial problem

---
+++ leetcode/RegularExpression/RegularExpressionSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.RegularExpression
+{
+    public class RegularExpressionSolution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            return false;
+        }
+    }
+}
---
+++ leetcodeTests/RegularExpression/RegularExpressionSolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.RegularExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.RegularExpression.Tests
+{
+    [TestClass()]
+    public class RegularExpressionSolutionTests
+    {
+        [TestMethod()]
+        public void IsMatchTest()
+        {
+            Assert.IsTrue(new RegularExpressionSolution().IsMatch("aab", "c*a*b"));
+        }
+    }
+}
