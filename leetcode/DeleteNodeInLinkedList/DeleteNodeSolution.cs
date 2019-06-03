b77d005606c07363b7b742c41f7d106039a6a37f
fuchun2018 <russel.chang@gmail.com>
2019-06-02 20:51:21 -0700

    init problem

---
+++ leetcode/DeleteNodeInLinkedList/DeleteNodeSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.DeleteNodeInLinkedList
+{
+    public class DeleteNodeSolution
+    {
+        public void DeleteNode(ListNode node)
+        {
+
+        }
+    }
+}
