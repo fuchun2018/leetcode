e223083982e30a0b802661638c66f9afed6d6cc9
fuchun2018 <russel.chang@gmail.com>
2019-06-03 11:02:07 -0700

    init problem

---
+++ leetcode/LongestConsecutive/LongestConsecutiveSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.LongestConsecutive
+{
+    public class LongestConsecutiveSolution
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            return 1;
+        }
+    }
+}
