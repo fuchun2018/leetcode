ec41d5bafe43264e8bb6fafe6c8000225d8e948f
fuchun2018 <russel.chang@gmail.com>
2019-06-02 21:01:41 -0700

    submit code, passed all test cases in leetcode. i don't like this problem. the problem design is ugly.

--- leetcode/DeleteNodeInLinkedList/DeleteNodeSolution.cs
+++ leetcode/DeleteNodeInLinkedList/DeleteNodeSolution.cs
@@ -10,7 +10,11 @@ namespace leetcode.DeleteNodeInLinkedList
     {
         public void DeleteNode(ListNode node)
         {
+            if (node == null || node.next == null)
+                return;
 
+            node.val = node.next.val;
+            node.next = node.next.next;
         }
     }
 }
