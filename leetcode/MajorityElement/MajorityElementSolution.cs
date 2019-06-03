9aa9e8569f08517ddf11319f95cd242d733b78b2
fuchun2018 <russel.chang@gmail.com>
2019-06-02 21:27:37 -0700

    first attemption, passed test cases. straightforward solution. create a list and sort by value. not a good solution. 9/100, 5/100

---
+++ leetcode/MajorityElement/MajorityElementSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.MajorityElement
+{
+    public class MajorityElementSolution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            SortedList<int, int> list = new SortedList<int, int>();
+                
+            foreach (var item in nums)
+            {
+                if (list.ContainsKey(item)) list[item]++;
+                else list.Add(item, 1);
+            }
+            return list.OrderByDescending(t => t.Value).First().Key;
+        }
+    }
+}
---
+++ leetcodeTests/MajorityElement/MajorityElementSolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.MajorityElement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.MajorityElement.Tests
+{
+    [TestClass()]
+    public class MajorityElementSolutionTests
+    {
+        [TestMethod()]
+        public void MajorityElementTest()
+        {
+            Assert.AreEqual(new MajorityElementSolution().MajorityElement(new int[] { 3, 2, 3 ,3,3,3,3,4,4,2,1,4,5,3,3,0}), 3);
+        }
+    }
+}
