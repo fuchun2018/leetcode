1dadc01e308e0f1a63b810cebb3a928be432df01
fuchun2018 <russel.chang@gmail.com>
2019-06-05 09:29:47 -0700

    submitted and passed all test cases in leetcode. easy recursive version like textbook 101. Leetcode should mark this problem 'Easy'. so I want go further to iterative version.

---
+++ leetcode/InorderTraversal/InorderTraversalSolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.InorderTraversal
+{
+    public class InorderTraversalSolution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null) return null;
+            if (root.left == null && root.right == null) return new List<int> { root.val };
+            var result = new List<int>();
+            if (root.left != null) result.AddRange(InorderTraversal(root.left));
+            result.Add(root.val);
+            if (root.right != null) result.AddRange(InorderTraversal(root.right));
+            return result;
+        }
+    }
+}
---
+++ leetcodeTests/InorderTraversal/InorderTraversalSolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.InorderTraversal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.InorderTraversal.Tests
+{
+    [TestClass()]
+    public class InorderTraversalSolutionTests
+    {
+        [TestMethod()]
+        public void InorderTraversalTest()
+        {
+            var input = new TreeNode(1);
+            input.right = new TreeNode(2);
+            input.right.left = new TreeNode(3);
+
+            var expect = new List<int>
+            {
+                1, 3, 2
+            };
+            Assert.AreEqual(expect, new InorderTraversalSolution().InorderTraversal(input));
+        }
+    }
+}
