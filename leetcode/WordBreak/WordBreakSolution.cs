647730795631f26f7b16729e6f98869badfd1f8c
fuchun2018 <russel.chang@gmail.com>
2019-06-08 11:46:17 -0700

    init problem

---
+++ leetcode/WordBreak/WordBreakSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.WordBreak
+{
+    public class WordBreakSolution
+    {
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            return null;
+        }
+    }
+}
---
+++ leetcodeTests/WordBreak/WordBreakSolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.WordBreak;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.WordBreak.Tests
+{
+    [TestClass()]
+    public class WordBreakSolutionTests
+    {
+        [TestMethod()]
+        public void WordBreakTest()
+        {
+            var solution = new WordBreakSolution();
+            var s = "catsanddog";
+            var dict = new string[] { "cat", "cats", "and", "sand", "dog" };
+            var expect = new List<string>
+            {
+                 "cats and dog",  "cat sand dog"
+            };
+            var result = solution.WordBreak(s, dict);
+            foreach (var item in result)
+            {
+                Assert.IsTrue(expect.Contains(item));
+            }
+        }
+    }
+}
