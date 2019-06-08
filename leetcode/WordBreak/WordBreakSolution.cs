3fa216c97199e1c04a7ba49608d244c33afa130d
fuchun2018 <russel.chang@gmail.com>
2019-06-08 14:04:18 -0700

    first attemption. create a dictionary for all worddictionary. then move string cursor from left to right. once find the match in the dictionary. put it into the wip. then put wip into result.

--- leetcode/WordBreak/WordBreakSolution.cs
+++ leetcode/WordBreak/WordBreakSolution.cs
@@ -8,9 +8,47 @@ namespace leetcode.WordBreak
 {
     public class WordBreakSolution
     {
+        private Dictionary<char, List<string>> dictionary;
         public IList<string> WordBreak(string s, IList<string> wordDict)
         {
-            return null;
+            var result = new List<string>();
+            if (string.IsNullOrEmpty(s) || wordDict.Count == 0) return result;
+            dictionary = new Dictionary<char, List<string>>();
+            foreach (var item in wordDict)
+            {
+                if (dictionary.ContainsKey(item[0]))
+                {
+                    var list = dictionary[item[0]];
+                    if (list.Contains(item) != true) dictionary[item[0]].Add(item);
+                }
+                else
+                    dictionary.Add(item[0], new List<string> { item });
+            }
+            var wip = new List<string>();
+            Util(s, 0, wip, result);
+            return result;
+        }
+
+        private void Util(string s, int cursor, List<string> wip, List<string> result)
+        {
+            if (cursor == s.Length)
+            {
+                result.Add(wip.Aggregate((a, b) => a + " " + b));
+                return;
+            }
+
+            var substring = s.Substring(cursor, s.Length - cursor);
+            if (dictionary.ContainsKey(substring[0]) != true) return;
+            var list = dictionary[substring[0]].Where(t => s.IndexOf(t) >= 0).ToList();
+            foreach (var item in list)
+            {
+                if (substring.IndexOf(item) == 0)
+                {
+                    wip.Add(item);
+                    Util(s, cursor + item.Length, wip, result);
+                    wip.Remove(item);
+                }
+            }
         }
     }
 }
--- leetcodeTests/WordBreak/WordBreakSolutionTests.cs
+++ leetcodeTests/WordBreak/WordBreakSolutionTests.cs
@@ -27,5 +27,37 @@ namespace leetcode.WordBreak.Tests
                 Assert.IsTrue(expect.Contains(item));
             }
         }
+        [TestMethod]
+        public void WordBreakTest_Case2()
+        {
+            var solution = new WordBreakSolution();
+            var s = "";
+            var dict = new string[] { "cat", "cats", "and", "sand", "dog" };
+
+            var result = solution.WordBreak(s, dict);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void WordBreakTest_Case3()
+        {
+            var solution = new WordBreakSolution();
+            var s = "catsanddog";
+            var dict = new string[] { "cat", "cats", "and" };
+
+            var result = solution.WordBreak(s, dict);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void WordBreakTest_Case4()
+        {
+            var solution = new WordBreakSolution();
+            var s = "pineapplepenapple";
+            var dict = new string[] { "apple", "pen", "applepen", "pine", "pineapple" };
+
+            var result = solution.WordBreak(s, dict);
+            Assert.AreNotEqual(0, result.Count);
+        }
     }
 }
