97ddc2993ea75470137518486642049b3b62b973
fuchun2018 <russel.chang@gmail.com>
2019-06-10 21:13:49 -0700

    init problem

---
+++ leetcode/ReverseLinkedList/ReverseListSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.ReverseLinkedList
+{
+    public class ReverseListSolution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            return null;
+        }
+    }
+}
