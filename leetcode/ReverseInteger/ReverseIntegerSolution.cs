0debbb3d231ede51a796181f6b870582b9407586
fuchun2018 <russel.chang@gmail.com>
2019-06-12 14:07:36 -0700

    submitted then return wrong answer in case 1027, because it need to * 10 to move to next digit. it may course overflow issue.

---
+++ leetcode/ReverseInteger/ReverseIntegerSolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.ReverseInteger
+{
+    public class ReverseIntegerSolution
+    {
+        public int Reverse(int x)
+        {
+            if (x == 0) return x;
+            var sign = x > 0;
+            var result = 0;
+            while (x != 0)
+            {
+                var remainder = x % 10;
+                result = result * 10 + remainder;
+                x /= 10;
+            }
+            return sign ? result : result * -1;
+        }
+    }
+}
---
+++ leetcodeTests/ReverseInteger/ReverseIntegerSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.ReverseInteger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.ReverseInteger.Tests
+{
+    [TestClass()]
+    public class ReverseIntegerSolutionTests
+    {
+        [TestMethod()]
+        public void ReverseTest()
+        {
+            var solution = new ReverseIntegerSolution();
+            //var result = 123 % 100;
+            //var res2 = 123 / 100;
+            //Assert.AreEqual(solution.Reverse(-123), -321);
+            solution.Reverse(1534236469);
+        }
+    }
+}
