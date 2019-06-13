b970f4818684e97172dccfedf1e2c73e935491e8
fuchun2018 <russel.chang@gmail.com>
2019-06-13 10:44:03 -0700

    init problem

---
+++ leetcode/SerializeAndDeserializeBinaryTree/SerializeAndDeserializeBinaryTreeSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.SerializeAndDeserializeBinaryTree
+{
+    public class SerializeAndDeserializeBinaryTreeSolution
+    {
+        // Encodes a tree to a single string.
+        public string serialize(TreeNode root)
+        {
+            return string.Empty;
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(string data)
+        {
+            return null;
+        }
+    }
+}
---
+++ leetcodeTests/SerializeAndDeserializeBinaryTree/SerializeAndDeserializeBinaryTreeSolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.SerializeAndDeserializeBinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.SerializeAndDeserializeBinaryTree.Tests
+{
+    [TestClass()]
+    public class SerializeAndDeserializeBinaryTreeSolutionTests
+    {
+        [TestMethod()]
+        public void serializeTest()
+        {
+            var solution = new SerializeAndDeserializeBinaryTreeSolution();
+            var treeNode = new TreeNode(1);
+            treeNode.left = new TreeNode(2);
+            treeNode.right = new TreeNode(3);
+            var result = solution.serialize(treeNode);
+            var expect = "1,2,,,3,,,";
+            Assert.AreEqual(expect, result);
+        }
+    }
+}
