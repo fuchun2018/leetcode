86181389a958149b9dec526cdeb2cec9943fd7c3
fuchun2018 <russel.chang@gmail.com>
2019-06-14 08:45:30 -0700

    init problem

---
+++ leetcode/LongestIncreasingPath/LongestIncreasingPathSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.LongestIncreasingPath
+{
+    public class LongestIncreasingPathSolution
+    {
+        public int LongestIncreasingPath(int[][] matrix)
+        {
+            return -1;
+        }
+    }
+}
---
+++ leetcodeTests/LongestIncreasingPath/LongestIncreasingPathSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.LongestIncreasingPath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.LongestIncreasingPath.Tests
+{
+    [TestClass()]
+    public class LongestIncreasingPathSolutionTests
+    {
+        [TestMethod()]
+        public void LongestIncreasingPathTest()
+        {
+            var solution = new LongestIncreasingPathSolution();
+            var input = new int[][] { new int[]{ 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } };
+            var result = solution.LongestIncreasingPath(input);
+            var expection = 4;
+            Assert.AreEqual(4, result);
+        }
+    }
+}
