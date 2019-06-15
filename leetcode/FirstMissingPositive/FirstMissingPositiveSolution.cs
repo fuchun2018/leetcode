7bc0b6849e81ab5b50351e43639f43ce08861635
fuchun2018 <russel.chang@gmail.com>
2019-06-15 12:44:06 -0700

    init problem

---
+++ leetcode/FirstMissingPositive/FirstMissingPositiveSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.FirstMissingPositive
+{
+    public class FirstMissingPositiveSolution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            return -1;
+        }
+    }
+}
---
+++ leetcodeTests/FirstMissingPositive/FirstMissingPositiveSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.FirstMissingPositive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.FirstMissingPositive.Tests
+{
+    [TestClass()]
+    public class FirstMissingPositiveSolutionTests
+    {
+        [TestMethod()]
+        public void FirstMissingPositiveTest()
+        {
+            var solution = new FirstMissingPositiveSolution();
+            var input = new int[] { 1, 2, 0 };
+            var expect = 3;
+            var result = solution.FirstMissingPositive(input);
+            Assert.AreEqual(expect, result);
+        }
+    }
+}
