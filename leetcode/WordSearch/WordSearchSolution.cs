e792e25beece2fb62c9636352c6e2f91d51d0ccf
fuchun2018 <russel.chang@gmail.com>
2019-06-23 20:27:54 -0700

    init problem

---
+++ leetcode/WordSearch/WordSearchSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.WordSearch
+{
+    public class WordSearchSolution
+    {
+        public IList<string> FindWords(char[][] board, string[] words)
+        {
+            return null;
+        }
+    }
+}
---
+++ leetcodeTests/WordSearch/WordSearchSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode.WordSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.WordSearch.Tests
+{
+    [TestClass()]
+    public class WordSearchSolutionTests
+    {
+        [TestMethod()]
+        public void FindWordsTest()
+        {
+            var solution = new WordSearchSolution();
+            var input = new char[][] { };
+            var sw = new string[] { };
+            var result = solution.FindWords(input, sw);
+            Assert.AreEqual(0,result.Count() );
+        }
+    }
+}
