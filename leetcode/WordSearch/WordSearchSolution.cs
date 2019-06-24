1a4bfc2c96f0688341c722ca01b3589057730408
fuchun2018 <russel.chang@gmail.com>
2019-06-23 21:51:52 -0700

    submitted and passed all test cases from leetcode. it is not a difficult problem but not a handy one.

--- leetcode/WordSearch/WordSearchSolution.cs
+++ leetcode/WordSearch/WordSearchSolution.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace leetcode.WordSearch
 {
@@ -10,8 +6,63 @@ namespace leetcode.WordSearch
     {
         public IList<string> FindWords(char[][] board, string[] words)
         {
-            if(words.Length == 0 || board.Length == 0) return new List<string>();
-            return null;
+            var result = new List<string>();
+            if (words.Length == 0 || board.Length == 0) return new List<string>();
+            var trie = Build(words);
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[0].Length; j++)
+                {
+                    TraversalBoard(i, j, trie, result, board);
+                }
+            }
+            return result;
+        }
+
+        private Trie Build(string[] words)
+        {
+            var result = new Trie();
+
+            foreach (var word in words)
+            {
+                var current = result;
+                foreach (var element in word)
+                {
+                    if (current.Key[element - 'a'] == null) current.Key[element - 'a'] = new Trie();
+                    current = current.Key[element - 'a'];
+                }
+
+                current.Word = word;
+            }
+
+            return result;
+        }
+
+        private void TraversalBoard(int x, int y, Trie trie, List<string> result, char[][] board)
+        {
+            var _ = board[x][y];
+            if (_ == '\0') return;
+            if (trie.Key[_ - 'a'] == null) return;
+            trie = trie.Key[_ - 'a'];
+            if (string.IsNullOrEmpty(trie.Word) != true && result.Contains(trie.Word) != true) result.Add(trie.Word);
+
+
+            board[x][y] = '\0';
+            if (y < board[0].Length - 1)
+                TraversalBoard(x, y + 1, trie, result, board);
+            if (y > 0)
+                TraversalBoard(x, y - 1, trie, result, board);
+            if (x < board.Length - 1)
+                TraversalBoard(x + 1, y, trie, result, board);
+            if (x > 0)
+                TraversalBoard(x - 1, y, trie, result, board);
+            board[x][y] = _;
+        }
+
+        private class Trie
+        {
+            public Trie[] Key { get; set; } = new Trie[26];
+            public string Word { get; set; }
         }
     }
 }
--- leetcodeTests/WordSearch/WordSearchSolutionTests.cs
+++ leetcodeTests/WordSearch/WordSearchSolutionTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using leetcode.WordSearch;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace leetcode.WordSearch.Tests
 {
@@ -18,7 +13,36 @@ namespace leetcode.WordSearch.Tests
             var input = new char[][] { };
             var sw = new string[] { };
             var result = solution.FindWords(input, sw);
-            Assert.AreEqual(0,result.Count() );
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod()]
+        public void FindWordsTest_Case2()
+        {
+            var solution = new WordSearchSolution();
+            var input = new[] { new[] { 's', 'e' }, new[] { 'r', 'a' } };
+            var sw = new[] { "sea", "ear" };
+            var result = solution.FindWords(input, sw);
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod()]
+        public void FindWordsTest_Case3()
+        {
+            var solution = new WordSearchSolution();
+            var input = new[] { new[] { 'a' } };
+            var sw = new[] { "a" };
+            var result = solution.FindWords(input, sw);
+            Assert.AreEqual(1, result.Count());
+        }
+        [TestMethod()]
+        public void FindWordsTest_Case4()
+        {
+            var solution = new WordSearchSolution();
+            var input = new[] { new[] { 'o', 'a', 'a', 'n' }, new[] { 'e', 't', 'a', 'e' }, new[] { 'i', 'h', 'k', 'r' }, new[] { 'i', 'f', 'l', 'v' } };
+            var sw = new[] { "oath", "pea", "eat", "rain" };
+            var result = solution.FindWords(input, sw);
+            Assert.AreEqual(2, result.Count());
         }
     }
 }
