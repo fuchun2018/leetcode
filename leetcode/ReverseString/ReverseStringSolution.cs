7914b532e64886df4fa1aeabf84639b751f7f57d
fuchun2018 <russel.chang@gmail.com>
2020-01-01 17:23:57 -0800

    init 124 Binary Tree Maximum Path Sum

--- leetcode/ReverseString/ReverseStringSolution.cs
+++ leetcode/ReverseString/ReverseStringSolution.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// https://leetcode.com/problems/reverse-string/
 namespace leetcode.ReverseString
 {
     public class ReverseStringSolution
@@ -12,12 +12,7 @@ namespace leetcode.ReverseString
         {
             if (s.Length < 2) return;
             int middle = s.Length / 2;
-            for (int i = 0; i < middle; i++)
-            {
-                var temp = s[i];
-                s[i] = s[s.Length - 1 - i];
-                s[s.Length - 1 - i] = temp;
-            }
+            for (var (i, j) = (0, s.Length - 1); i < j; ++i, --j) (s[i], s[j]) = (s[j], s[i]);
         }
     }
 }
---
+++ leetcode/ThreeSum/MaximumPathSumSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.ThreeSum
+{
+    public class MaximumPathSumSolution
+    {
+        public int MaxPathSum(TreeNode root)
+        {
+            if (root == null) return 0;
+            int[] maxPath = new[] { int.MinValue };
+
+            Dfs(root, maxPath);
+            return maxPath[0];
+        }
+
+        private int Dfs(TreeNode node, int[] maxVal)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            int left = Dfs(node.left, maxVal);
+            int right = Dfs(node.right, maxVal);
+            int myself = node.val + left + right;
+            maxVal[0] = Math.Max(myself, maxVal[0]);
+            return Math.Max(node.val + Math.Max(left, right), 0);
+        }
+    }
+}
